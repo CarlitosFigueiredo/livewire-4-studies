cedcb3058ed7b4f773603358d2a35a4228349ed1
Carlos Figueiredo <carlos.yuri@itcnet.com.br>
2026-01-14 17:02:56 -0300

    Installing Flux

--- resources/views/pages/post/⚡create.blade.php
+++ resources/views/pages/post/⚡create.blade.php
@@ -22,28 +22,12 @@ new #[Layout('layouts::app', ['title' => 'Create post'])] class extends Componen
 ?>
 
 <form wire:submit="save" class="w-96 space-y-6">
-    <label class="block space-y-2">
-        <p
-            class="inline-flex items-center text-sm font-medium [:.where(&)]:text-zinc-800 [::where(&)]:dark:text-white [&:has([aria-invalid='true'])]:text-red-500">
-            Title
-        </p>
-        <input type="text"
-            class="w-full border rounded-lg block disabled:shadow-none dark:shadow-none appearance-none text-" />
-    </label>
 
-    <label class="block space-y-2">
-        <p
-            class="inline-flex items-center text-sm font-medium [:.where(&)]:text-zinc-800 [::where(&)]:dark:text-white [&:has([aria-invalid='true'])]:text-red-500">
-            Content
-        </p>
-        <textarea
-            class="block p-3 w-full shadow-xs disabled:shadow-none border rounded-lg bg-white dark:bg-dark"></textarea>
-    </label>
+    <flux:input wire:model="title" label="Title" />
 
-    <button type="submit"
-        class="relative items-center font-medium justify-center gap-2 whitespace-nowrap disabled:opacity-7">
-        Save
-    </button>
+    <flux:textarea  wire:model="content" label="Content" />
+
+    <flux:button type="submit" variant="primary">Save</flux:button>
 </form>
 
 
